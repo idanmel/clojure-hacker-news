a88661f7ea33197bc28074e6a2d823fd243db5cd
Idan Melamed <idanmel@gmail.com>
2022-05-29 13:59:20 +0300

    extract mock request function

--- test/clojure_hacker_news/core_test.clj
+++ test/clojure_hacker_news/core_test.clj
@@ -4,20 +4,20 @@
             [ring.mock.request :as mock]
             [clojure-hacker-news.core :refer [content-type-plain-text myapp response-not-found]]))
 
+(defn mock-request [path]
+  (myapp (mock/request :get path)))
+
 (deftest ping
   (testing "ping"
-    (let [{status :status body :body headers :headers} (myapp (mock/request :get "/ping"))]
+    (let [{status :status body :body headers :headers} (mock-request "/ping")]
       (is (= 200 status))
       (is (= content-type-plain-text headers))
       (is (= "pong" body)))))
 
 (deftest not-found
   (testing "not-a-ping"
-    (is (= response-not-found (myapp (mock/request :get "/not-a-ping")))))
+    (is (= response-not-found (mock-request "/not-a-ping"))))
   (testing "not-found"
-    (is (= response-not-found (myapp (mock/request :get "/not-found")))))
+    (is (= response-not-found (mock-request "/not-found"))))
   (testing "also-not-found"
-    (is (= response-not-found (myapp (mock/request :get "/also-not-found"))))))
-
-
-
+    (is (= response-not-found (mock-request "/also-not-found")))))
