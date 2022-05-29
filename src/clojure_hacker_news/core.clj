f92483791093fe3c165d05f05e84433669222378
Idan Melamed <idanmel@gmail.com>
2022-05-29 12:16:09 +0300

    ping and 404

--- src/clojure_hacker_news/core.clj
+++ src/clojure_hacker_news/core.clj
@@ -1,6 +1,22 @@
-(ns clojure-hacker-news.core)
+(ns clojure-hacker-news.core
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found]]
+            [org.httpkit.server :refer [run-server]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn json-headers [body]
+  {:headers {"Content-Type" "application/json; charset=utf-8"}
+   :status 200
+   :body body})
+
+(defn plain-text-headers [body status]
+  {:headers {"Content-Type" "text/plain; charset=utf-8"}
+   :status status
+   :body body})
+
+(defroutes myapp
+           (GET "/ping" [] (plain-text-headers "pong" 200))
+           (GET "/not-a-ping" [] (plain-text-headers "not found" 404)))
+
+(defn -main []
+  (run-server myapp {:port 5000}))
--- test/clojure_hacker_news/core_test.clj
+++ test/clojure_hacker_news/core_test.clj
@@ -1,7 +1,21 @@
 (ns clojure-hacker-news.core-test
   (:require [clojure.test :refer :all]
-            [clojure-hacker-news.core :refer :all]))
+            [clojure-hacker-news.core :refer :all]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest ping
+  (testing "ping"
+    (is (= (myapp (mock/request :get "/ping"))
+           {:body    "pong"
+            :headers {"Content-Type" "text/plain; charset=utf-8"}
+            :status  200}))))
+
+(deftest not-found
+  (testing "not-a-ping")
+  (is (= (myapp (mock/request :get "/not-a-ping"))
+         {:body    "not found"
+          :headers {"Content-Type" "text/plain; charset=utf-8"}
+          :status  404})))
+
+
+
