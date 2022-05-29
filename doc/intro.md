60f49c9b7ae160f8b1a7a9ae48c822977c2b9e91
Idan Melamed <idanmel@gmail.com>
2022-05-29 09:49:52 +0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-hacker-news
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_hacker_news/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-hacker-news.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-hacker-news.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
