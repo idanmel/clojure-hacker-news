7c5e8db2dd66c5293972ed04817f5e76301c5063
Idan Melamed <idanmel@gmail.com>
2022-06-01 13:33:21 +0300

    kondo

--- test/clojure_hacker_news/functional_tests.clj
+++ test/clojure_hacker_news/functional_tests.clj
@@ -1,7 +1,8 @@
 (ns clojure-hacker-news.functional_tests
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [clojure-hacker-news.core :refer [start-server stop-server]]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [clojure-hacker-news.migrations :as migrations]))
 
 (deftest POST-create
   (testing "We can create a post by the correct call to the server"
@@ -12,6 +13,8 @@
 
 (defn server-fixture
   [f]
+  (migrations/init)
+  (migrations/migrate)
   (start-server 5000)
   (f)
   (stop-server 0))
